8f7bd08ef8c2041c40b9ddb0a548eb4377c35da3
Jaysig <jasontsigmon@gmail.com>
2016-02-09 12:31:17 -0500

    nothing displays

--- collections/recipes.js
+++ collections/recipes.js
@@ -14,6 +14,9 @@ RecipeSchema = new SimpleSchema({
 		label: "Author",
 		autoValue: function(){
 			return this.userId;
+		},
+		autoform:{
+			type: "hidden"
 		}
 	},
 	createdAt: {
@@ -21,6 +24,9 @@ RecipeSchema = new SimpleSchema({
 		label: "Created At",
 		autoValue: function(){
 			return new Data();
+		},
+		autoform:{
+			type: "hidden"
 		}
 	}
 });
